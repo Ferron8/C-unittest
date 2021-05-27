dad64aa6d67a272c811c5def704302b69ee65a27
Ferron8 <ferron383@gmail.com>
2021-05-27 18:05:42 -0500

    Add files via upload

---
+++ Reservation.cs
@@ -0,0 +1,21 @@
+﻿namespace TestNinja.Fundamentals
+{
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user) //method I want to test
+        {
+            return (user.IsAdmin || MadeBy == user); // 3 different execution paths
+                
+
+  
+        } 
+        
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ ReservationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests // same name as project add .UnitTests when saving
+{
+    [TestClass]
+    public class ReservationTests // name of class + test
+    {
+        [TestMethod] // test class attribute
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue() // method being tested_scenario_expected behavior
+        {
+
+            // Arrange: init objects
+            var reservation = new Reservation();
+
+            // Act: act on the object
+            result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert: verify result is true
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnsTrue()
+        {
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+    }
+}
